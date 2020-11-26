d75ae1191ccf8b8b667d9b472aa605ab765b566c
unknown <thschenk143@gmail.com>
2020-11-26 14:15:36 +0100

    no error codes oop

--- Class experimentation OOP/src/main.cpp
+++ Class experimentation OOP/src/main.cpp
@@ -1,9 +1,59 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+class ledHandling
+{
+  public: 
+  ledHandling(int pin);
+  void begin();
+  void aan();
+  void stop();
+  String statusVanled();
+
+  private:
+  int pin;
+  String status;
+
+};
+
+ledHandling::ledHandling(int pin)
+{
+  pin = pin;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void ledHandling::begin()
+{
+  pinMode(pin, OUTPUT);
+}
+
+void ledHandling::aan()
+{
+  digitalWrite(pin, HIGH);
+  status = "Aan";
+}
+
+void ledHandling::stop()
+{
+  digitalWrite(pin, LOW);
+  status = "Uit";
+}
+
+String ledHandling::statusVanled()
+{
+  return status;
+}
+
+ledHandling Led1(10);
+
+void setup() 
+{
+  Serial.begin(9600);
+  Led1.begin();
+}
+
+void loop() 
+{
+  Led1.statusVanled();
+  Led1.aan();
+  Led1.statusVanled();
+  Led1.stop();
 }
