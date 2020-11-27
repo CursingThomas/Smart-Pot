616872fd811a1af0d1a949080dc5d6e4f4905cb0
unknown <thschenk143@gmail.com>
2020-11-27 13:50:46 +0100

    added cpp file

--- Class experimentation OOP/include/lichtsterkte.h
+++ Class experimentation OOP/include/lichtsterkte.h
@@ -14,35 +14,3 @@ class lichtsterkte
   char strprocessedData;
   int rawData;
 };
-
-lichtsterkte::lichtsterkte(int pin)
-{
-  pin = pin;
-}
-
-void lichtsterkte::begin()
-{
-  pinMode(pin, INPUT);
-}
-
-int lichtsterkte::pullData()
-{
-  rawData = analogRead(pin);
-
-  return rawData;
-}
-
-int lichtsterkte::processData()
-{
-  processedData = (rawData * 100) / 4095;
-
-  return processedData;
-}
-
-char lichtsterkte::printData()
-{
-  strprocessedData = processedData + strprocessedData;
-  Serial.print(processedData);
-
-  return strprocessedData;
-}
---
+++ Class experimentation OOP/src/lichtsterkte.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+#include "lichtsterkte.h"
+
+lichtsterkte::lichtsterkte(int pin)
+{
+  pin = pin;
+}
+
+void lichtsterkte::begin()
+{
+  pinMode(pin, INPUT);
+}
+
+int lichtsterkte::pullData()
+{
+  rawData = analogRead(pin);
+
+  return rawData;
+}
+
+int lichtsterkte::processData()
+{
+  processedData = (rawData * 100) / 4095;
+
+  return processedData;
+}
+
+char lichtsterkte::printData()
+{
+  strprocessedData = processedData + strprocessedData;
+  Serial.print(processedData);
+
+  return strprocessedData;
+}
