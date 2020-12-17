7d24af143b81e542460c7bab9646fd577114be84
Thomas Schenk <thschenk143@gmail.com>
2020-12-17 16:09:44 +0100

    Added code description

--- Smart-Pot-CPP/src/main.cpp
+++ Smart-Pot-CPP/src/main.cpp
@@ -1,4 +1,4 @@
-// Includen van alle benodigde libraries
+//* Include Third-party libraries.
 #include <Arduino.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
@@ -6,6 +6,7 @@
 #include <ESP32Servo.h>
 #include <LiquidCrystal.h>
 
+//* Include my classes.
 #include <MyDHT.h>
 #include <ultrasonicSensor.h>
 #include <grondVochtigheid.h>
@@ -14,37 +15,15 @@
 #include <waterPump.h>
 #include <PotentioMeter.h>
 
-
+//* Define type of DHT sensor (define instead of const because library asks for a define).
 #define DHTTYPE DHT11
 
-/*
-// Const int's van pins
-const int DHTPIN = 14;
-const int servopin = 15;
-const int echoPin = 26;
-const int grondPin = 33;
-const int lichtPin1 = 32;
-const int lichtPin2 = 35;
-const int lichtPin3 = 34;
-const int greenpin = 2;
-const int redpin = 4;
-const int trigPin = 27;
-
-const int vo = 23;
-const int Rs = 22;
-const int E = 1;
-const int D4 = 3;
-const int D5 = 21;
-const int D6 = 19;
-const int D7 = 18;
-*/
-
-// magic number handlers
+//* Magic number variables.
 const int honderdWaarde = 100;
 const int maxwaardeSensor = 4095;
 const int DHTPIN = 14;
 
-// Class declaraties
+//* Initializing all objects.
 ultrasonicSensor ultrasonicSensor1(27, 26);
 grondVochtigheid grondVochtigheidsSensor1(33);
 ledController RGBLed1(2,4);
@@ -57,8 +36,7 @@ PotentioMeter potMeter1(1000); // zoek nog een pin!
 
 StaticJsonDocument<200> doc;
 
-// Declaratie van WiFi netwerken die gebruikt kunnen worden
-
+//* Declaring usable Wi-Fi networks.
 const char* ssid = "schenktol";
 const char* password = "TLE143TLE";
 
@@ -68,11 +46,10 @@ const char* password = "TLE143TLE";
 //const char* ssid = "12connect";
 //const char* password = "";
 
-// Declaratie van de eindbestemming
+//* Declaring the endpoint of the HTTP Post Request.
 const char* serverName = "http://smartpot.nealgeilen.nl/api";
 
-//const char* serverName = "https://collect2.com/api/02f27a5d-b70b-4cc2-b451-fd9e89be984f/datarecord/";
-
+//* Function made to control a servo (functioning as a pump) which has dependencies with the grondVochtigheid class.
 void giveWaterServo()
 {
   int servoPin = 15;
@@ -88,6 +65,7 @@ void giveWaterServo()
   }
 }
 
+//* Function made to control a pump which has dependencies with the grondVochtigheid class.
 void giveWaterPomp()
 {
   const int potpin = 10;
@@ -102,6 +80,7 @@ void giveWaterPomp()
   }  
 }
 
+//* Function made to control a RGB LED which has dependencies with the UltrasonicSensor class.
 void setLedStatus()
 {
   int distance = ultrasonicSensor1.getRawData();
@@ -117,103 +96,104 @@ void setLedStatus()
   }
 }
 
+//* Normal void setup()
 void setup() 
 {
-  Serial.begin(9600);
-  WiFi.begin(ssid, password);
-  Serial.println("Connecting");
-  LCD.begin(16,1);
-  while(WiFi.status() != WL_CONNECTED) 
+  Serial.begin(9600); // Set baudrate to 9600.
+  LCD.begin(16,1); // Call to class function . begin with parameters to determine the size of the LCD screen.
+  WiFi.begin(ssid, password); // Call to class function .begin with parameters ssid and password.
+  Serial.println("Connecting"); // When the above line of code is executed, print that the MC is connecting to WiFi.
+  while(WiFi.status() != WL_CONNECTED // While WiFi is not connected, print out a . to indicate the microcontroller is trying to connect to the WiFi
   {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("");
-  Serial.print("Verbonden met WiFi, met Ip adres: ");
+  Serial.println(""); // Print a clear line in the console
+  Serial.print("Verbonden met WiFi, met Ip adres: "); // Print that the microcontroller has succesfully connected to the internet with the following ip address.
   Serial.println(WiFi.localIP());
 }
 
+//* Normal void loop()
 void loop()
 {
-  const int timerDelaywaarde = 1000;
-  unsigned long lastTime = 0;
-  unsigned long timerDelay = timerDelaywaarde;
-  setLedStatus();
-  giveWaterServo();
+  const int timerDelaywaarde = 1000; // Sets the amount of time the interval between two HTTP Post Request.
+  unsigned long lastTime = 0; // Sets variable used to calculate if timing has been met.
+  unsigned long timerDelay = timerDelaywaarde; // Check two variables.
+  setLedStatus(); // Executes this function.
+  giveWaterServo(); // Executes this function.
 
-  if ((millis() - lastTime) > timerDelay) 
+  if ((millis() - lastTime) > timerDelay) // Sets if statement to check if requirements are met.
   {
    
-    if(WiFi.status()== WL_CONNECTED)
+    if(WiFi.status()== WL_CONNECTED) // Sets if statement to check if requirements are met.
     {
-      //Lees alle sensoren uit
-
-      //ultrasone sensor
-      ultrasonicSensor1.getRawData();
+      //* Gathers raw data of all sensors.
+      ultrasonicSensor1.getRawData(); // Gathers raw data of ultrasonic sensor.
       
-      // DHT sensor
-      double luchtVochtigheid = dht.readHumidity();
-      double temperatuur = dht.readTemperature();
+      double luchtVochtigheid = dht.readHumidity(); // Gathers raw data of DHT11 sensor.
+      double temperatuur = dht.readTemperature(); // Gathers raw data of DHT11 sensor.
       
+      grondVochtigheidsSensor1.getMoisture(); // Gathers raw data of soil moisture sensor.
 
-      // Grondvochtigheids sensor
-      grondVochtigheidsSensor1.getMoisture();
+      lichtSensor1.pullData(); // Gathers raw data of LDR1.
+      lichtSensor2.pullData(); // Gathers raw data of LDR2.
+      lichtSensor2.pullData(); // Gathers raw data of LDR3.
+    
+      //* Compute raw data gathered before to processed data which is representative.
+      ultrasonicSensor1.processData(); // Computes raw data to processed data from ultrasonic sensor.
 
-      // Lichtsterkte sensoren
-      lichtSensor1.pullData();
-      lichtSensor2.pullData();
-      lichtSensor2.pullData();
+      grondVochtigheidsSensor1.processMoistureToPercent(); // Computes raw data to processed data from soil moisture sensor.
+
+      lichtSensor1.processData(); // Computes raw data to processed data from LDR1.
+      lichtSensor2.processData(); // Computes raw data to processed data from LDR2.
+      lichtSensor3.processData(); // Computes raw data to processed data from LDR3.
+
+      //* Print out all the raw data gathered.
+      dht.printTemperatuur(); // Prints raw data from DHT11.
+      dht.printLuchtVochtigheid(); // Prints raw data from DHT11.
+
+      ultrasonicSensor1.printRawData(); // Prints raw data from ultrasonic sensor.
+
+      grondVochtigheidsSensor1.printRawData(); // Prints raw data from soil moisture sensor.
+
+      lichtSensor1.printRawData(); // Prints raw data from LDR1.
+      lichtSensor2.printRawData(); // Prints raw data from LDR2.
+      lichtSensor3.printRawData(); // Prints raw data from LDR3.
+
+      //* Print all final computed data which will be posted to the website.
+      ultrasonicSensor1.printProcessedData(); // Prints processed data from ultrasonic sensor.
+
+      grondVochtigheidsSensor1.printProcessedData(); // Prints processed data from soil moisture sensor.
+
+      lichtSensor1.printProcessedData(); // Prints processed data from LDR1.
+      lichtSensor2.printProcessedData(); // Prints processed data from LDR2.
+      lichtSensor3.printProcessedData(); // Prints processed data from LDR3.
       
-      // Procentuele omrekening van sensorwaardes
-      ultrasonicSensor1.processData();
-      grondVochtigheidsSensor1.processMoistureToPercent();
-      lichtSensor1.processData();
-      lichtSensor2.processData();
-      lichtSensor3.processData();
-
-      // Printen van alle onbewerkte variabelen
-      dht.printTemperatuur();
-      dht.printLuchtVochtigheid();
-      ultrasonicSensor1.printRawData();
-      grondVochtigheidsSensor1.printRawData();
-      lichtSensor1.printRawData();
-      lichtSensor2.printRawData();
-      lichtSensor3.printRawData();
-
-      // Print alle waardes uit die gepost worden
-
-
-      ultrasonicSensor1.printProcessedData();
-      grondVochtigheidsSensor1.printProcessedData();
-      lichtSensor1.printProcessedData();
-      lichtSensor2.printProcessedData();
-      lichtSensor3.printProcessedData();
+      //* Converts processed data to a datatype of String.
+      String distance1 = ultrasonicSensor1.dataToString(); // Converts processed data from ultrasonic sensor to datatype String.
+      String strGrondVochtigheid1 = grondVochtigheidsSensor1.dataToString(); // Converts processed data from soil moisture sensor to datatype String.
+      String strLichtSterkte1 = lichtSensor1.dataToString(); // Converts processed data from LDR1 to datatype String.
+      String strLichtSterkte2 = lichtSensor2.dataToString(); // Converts processed data from LDR2 to datatype String.
+      String strLichtSterkte3 = lichtSensor3.dataToString(); // Converts processed data from LDR3 to datatype String.
+      String strTemperatuur = dht.dataToString(temperatuur); // Converts processed data from DHT11 to datatype String.
+      String strLuchtVochtigheid = dht.dataToString(luchtVochtigheid); // Converts processed data from DHT11 to datatype String.
       
-      // Data naar string
-      String distance1 = ultrasonicSensor1.dataToString();
-      String strGrondVochtigheid1 = grondVochtigheidsSensor1.dataToString();
-      String strLichtSterkte1 = lichtSensor1.dataToString();
-      String strLichtSterkte2 = lichtSensor2.dataToString();
-      String strLichtSterkte3 = lichtSensor3.dataToString();
-      String strTemperatuur = dht.dataToString(temperatuur);
-      String strLuchtVochtigheid = dht.dataToString(luchtVochtigheid);
-      
-      int contrast = 75;
-      LCD.setCursor(0, 0);
-      LCD.print("Temperatuur: " + String(temperatuur));
+      int contrast = 75; // Sets contrast for LCD screen.
+      LCD.setCursor(0, 0); // Sets cursor on LCD screen to home.
+      LCD.print("Temperatuur: " + String(temperatuur)); // Prints the temperature on the LCD screen.
 
+      //* HTTP Post Request
+      HTTPClient http; // Initializes object.
+      http.begin(serverName); // Calls to function .begin with parameter serverName.
+      http.addHeader("Content-Type", "application/json"); // Adds a header to the HTTP Post Request.
+      http.addHeader("X-AUTH-TOKEN", "TEST"); // Adds a header to the HTTP Post Request.
+      http.addHeader("X-AUTH-ID", "wajdhlawkjhdlawjkdhawkjdh"); // Adds a header to the HTTP Post Request.  
+      int httpResponseCode = http.POST("{ \"Temperatuur\": "+strTemperatuur+" , \"Luchtvochtigheid\": "+strLuchtVochtigheid+" , \"Grondvochtigheid\": "+strGrondVochtigheid1+" , \"Waterniveau\": "+distance1+" , \"Lichtsterkte1\": "+strLichtSterkte1+" , \"Lichtsterkte2\" : "+strLichtSterkte2+" , \"Lichtsterkte3\" : "+strLichtSterkte3+"}"); // Send the actual HTTP Post Request.
+      String response = http.getString(); // Gather the response the website gives.
+      Serial.print("HTTP Response code: "); // Print message.
+      Serial.println(httpResponseCode); // print the actual response the website gives.
       
-      HTTPClient http;
-      http.begin(serverName);
-      http.addHeader("Content-Type", "application/json");
-      http.addHeader("X-AUTH-TOKEN", "TEST");
-      http.addHeader("X-AUTH-ID", "wajdhlawkjhdlawjkdhawkjdh");      
-      int httpResponseCode = http.POST("{ \"Temperatuur\": "+strTemperatuur+" , \"Luchtvochtigheid\": "+strLuchtVochtigheid+" , \"Grondvochtigheid\": "+strGrondVochtigheid1+" , \"Waterniveau\": "+distance1+" , \"Lichtsterkte1\": "+strLichtSterkte1+" , \"Lichtsterkte2\" : "+strLichtSterkte2+" , \"Lichtsterkte3\" : "+strLichtSterkte3+"}");
-      String response = http.getString();
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
-      
-      // Schoon alles op
+      //* Clear all strings to use them next time.
       distance1 = ultrasonicSensor1.clearString();
       strGrondVochtigheid1 = grondVochtigheidsSensor1.clearString();
       strLichtSterkte1 = lichtSensor1.clearString();
@@ -222,13 +202,13 @@ void loop()
       strTemperatuur = dht.clearString(strTemperatuur);
       strLuchtVochtigheid = dht.clearString(strLuchtVochtigheid);
 
-      http.end();
+      http.end(); // End the HTTP Post Request.
     }
     
     else 
     {
-      Serial.println("WiFi Disconnected");
+      Serial.println("WiFi Disconnected"); // Prints message if WiFi gets disconnected.
     }   
-    lastTime = millis();
+    lastTime = millis(); // Make sure if statement for all code above stays valid.
   }
 }
